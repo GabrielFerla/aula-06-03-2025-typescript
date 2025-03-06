1365a60e7b012998cc6e117d0df3e40fefd407ea
Ferla <ferlagamer@hotmail.com>
2025-03-06 08:27:11 -0300

    Exercícios de TypeScript avançado -- Carro

---
+++ index.js
@@ -0,0 +1,7 @@
+var meuCarro = {
+    marca: "chevrolet",
+    modelo: "chevette",
+    ano: 2002,
+    motor: "1.6 Flex"
+};
+console.log("Meu Carro:", meuCarro);
---
+++ index.ts
@@ -0,0 +1,14 @@
+// 1. Interfaces e Tipagem Avançada
+interface Carro {
+    marca: string;
+    modelo: string;
+    ano: number;
+    motor?: string;
+}
+const meuCarro: Carro = {
+    marca: "chevrolet",
+    modelo: "chevette",
+    ano: 2002,
+    motor: "1.6 Flex"
+};
+console.log("Meu Carro:", meuCarro);
